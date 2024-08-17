c9e218736eb34cc8a10562e61632f61ba8dfd08c
HMiguelvilchez <miguelvilchez270910@gmail.com>
2024-08-17 13:23:56 -0500

    Add  configuration24

--- src/views/admin/AppointmentsView.vue
+++ src/views/admin/AppointmentsView.vue
@@ -41,6 +41,13 @@ watch(selectedDate, handleDateChange)
     <p class="text-white text-lg mt-5">
         A continuación podrás administrar tus próximas citas
     </p>
+    <button
+                      type="button"
+                      class="bg-red-600 hover:bg-red-700 p-2 text-white uppercase text-xs font-extrabold rounded-lg"
+                      @click="user.logout"
+                  >
+                      Cerrar Sesión
+                  </button>
 
     <!-- Selector de fecha -->
     <div class="mt-5">
