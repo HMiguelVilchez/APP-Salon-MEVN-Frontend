7db5f407bc3588b1ecc1341ddba3f5fbc308876d
Miguel vilchez <miguelvilchez270910@gmail.com>
2024-08-27 14:45:58 -0500

    Add configuration40

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="spinner-container">
+      <div class="spinner"></div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LoadingSpinner',
+  };
+  </script>
+  
+  <style scoped>
+  .spinner-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #ffffff;
+    z-index: 9999;
+  }
+  
+  .spinner {
+    width: 100px; /* Tamaño más grande */
+    height: 100px; /* Tamaño más grande */
+    border: 8px solid rgba(0, 0, 0, 0.1); /* Bordes más gruesos */
+    border-top-color: #3498db;
+    border-radius: 50%;
+    animation: spin 1s linear infinite;
+  }
+  
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  </style>
+  
--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,26 +1,31 @@
 <script setup>
-import { inject } from "vue";
-import {useRouter} from 'vue-router'
+import { ref, inject } from "vue";
+import { useRouter } from 'vue-router';
 import AuthAPI from "../../api/AuthAPI";
 
 const toast = inject("toast");
-const router = useRouter()
+const router = useRouter();
+const isLoading = ref(false); // Estado de carga
 
 const handleSubmit = async (formData) => {
+  isLoading.value = true; // Inicia el loading spinner
   try {
     const {
       data: { token },
     } = await AuthAPI.login(formData);
     localStorage.setItem("AUTH_TOKEN", token);
-    router.push({name:'my_appointments'})
+    router.push({ name: 'my_appointments' }); // Redirige a la página de citas
   } catch (error) {
     toast.open({
       message: error.response.data.msg,
       type: "error",
     });
+  } finally {
+    isLoading.value = false; // Detén el loading spinner
   }
 };
 </script>
+
 
 <template>
   <h1 class="text-6xl font-extrabold text-white text-center mt-10">
@@ -43,6 +48,7 @@ const handleSubmit = async (formData) => {
       name="email"
       placeholder="Email de Usuario"
       validation="required|email"
+      autocomplete="username"
       :validation-messages="{
         required: 'El Email es obligatorio',
         email: 'Email no válido',
@@ -54,12 +60,14 @@ const handleSubmit = async (formData) => {
       name="password"
       placeholder="Password de Usuario"
       validation="required"
+      autocomplete="current-password"
       :validation-messages="{
         required: 'El Password es obligatorio',
       }"
     />
-
     <FormKit type="submit">Iniciar Sesión</FormKit>
   </FormKit>
 </template>
+
 <style scoped></style>
+
